5e992c51d61bbadf87a056f4ff2d1b09689ff264
Sweta Sona <mscod3r@gmail.com>
2025-06-16 08:33:29 +0530

    Remove unnecessary scripts, keeping only essential ones referenced in package.json

--- scripts/populate-company-field.sql
+++
@@ -1,6 +0,0 @@
--- Populate the company field with company names for existing users
-UPDATE users 
-SET company = companies.name
-FROM companies
-WHERE users.company_id = companies.id
-AND users.company IS NULL; 
--- scripts/refresh-browser.js
+++
@@ -1,3 +0,0 @@
-console.log("Candidates have been successfully deleted from the database.");
-console.log("Please refresh your browser to see the changes.");
-console.log("You can refresh by pressing F5 or clicking the refresh button in your browser."); 
